66dfbc873149897a76e15e4db486bbdaa4b543d1
Hubert Aschermann <aschehub@fit.cvut.cz>
2022-11-06 23:46:09 +0100

    dao: AuthorRepository

--- src/main/java/haschman/library_server/dao/AuthorRepository.java
+++ src/main/java/haschman/library_server/dao/AuthorRepository.java
@@ -1,6 +1,16 @@
 package haschman.library_server.dao;
 
+import haschman.library_server.domain.Author;
 import haschman.library_server.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-public interface AuthorRepository extends CrudRepository<User, Long> {
+import java.util.Collection;
+
+@Repository
+public interface AuthorRepository extends CrudRepository<User, Long>, JpaRepository<User, Long> {
+    @Query(value = "select a from Author a join Book b where b.id = :bookId")
+    Collection<Author> findAllAuthorsOfBook(@Param("bookId") Long bookId);
 }
