b2a53d5308fbd6911156987f00f38bc28e6c9119
Hubert Aschermann <aschehub@fit.cvut.cz>
2022-11-17 22:33:49 +0100

    api.model: LocationDTO

---
+++ src/main/java/haschman/library_server/api/model/LocationDTO.java
@@ -0,0 +1,31 @@
+package haschman.library_server.api.model;
+
+public class LocationDTO {
+    Long id;
+    int stand;
+    int shelf;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getStand() {
+        return stand;
+    }
+
+    public void setStand(int stand) {
+        this.stand = stand;
+    }
+
+    public int getShelf() {
+        return shelf;
+    }
+
+    public void setShelf(int shelf) {
+        this.shelf = shelf;
+    }
+}
