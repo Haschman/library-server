0869739274df47566b4741de3556f1c33c116c00
Hubert Aschermann <aschehub@fit.cvut.cz>
2022-11-17 20:36:56 +0100

    api: AbstractCrudController update

--- src/main/java/haschman/library_server/api/AbstractCrudController.java
+++ src/main/java/haschman/library_server/api/AbstractCrudController.java
@@ -9,23 +9,23 @@ import java.util.function.Function;
 
 public class AbstractCrudController<E extends DomainEntity<ID>, D, ID> {
     protected AbstractCrudService<E, ID> service;
-    protected Function<E, D> toDtoConverter;
+    protected Function<E, D> toDTOConverter;
     protected Function<D, E> toEntityConverter;
 
-    public AbstractCrudController(AbstractCrudService<E, ID> service, Function<E, D> toDtoConverter, Function<D, E> toEntityConverter) {
+    public AbstractCrudController(AbstractCrudService<E, ID> service, Function<E, D> toDTOConverter, Function<D, E> toEntityConverter) {
         this.service = service;
-        this.toDtoConverter = toDtoConverter;
+        this.toDTOConverter = toDTOConverter;
         this.toEntityConverter = toEntityConverter;
     }
 
     @PostMapping
     public D create(@RequestBody D entityAsDTO) {
-        return toDtoConverter.apply(service.create(toEntityConverter.apply(entityAsDTO)));
+        return toDTOConverter.apply(service.create(toEntityConverter.apply(entityAsDTO)));
     }
 
     @GetMapping
     public Collection<D> readAll() {
-        return service.readAll().stream().map(toDtoConverter).toList();
+        return service.readAll().stream().map(toDTOConverter).toList();
     }
 
     @PutMapping("/{id}")
