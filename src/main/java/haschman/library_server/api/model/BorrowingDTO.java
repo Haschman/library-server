191b4742360d2489c2d54bf77293507e61b1ea16
Hubert Aschermann <aschehub@fit.cvut.cz>
2022-11-17 22:32:22 +0100

    api.model: BorrowingDTO

---
+++ src/main/java/haschman/library_server/api/model/BorrowingDTO.java
@@ -0,0 +1,51 @@
+package haschman.library_server.api.model;
+
+import java.text.SimpleDateFormat;
+
+public class BorrowingDTO {
+    Long id;
+    String bookName;
+    String userName;
+    SimpleDateFormat date;
+    SimpleDateFormat returnDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public SimpleDateFormat getDate() {
+        return date;
+    }
+
+    public void setDate(SimpleDateFormat date) {
+        this.date = date;
+    }
+
+    public SimpleDateFormat getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(SimpleDateFormat returnDate) {
+        this.returnDate = returnDate;
+    }
+}
