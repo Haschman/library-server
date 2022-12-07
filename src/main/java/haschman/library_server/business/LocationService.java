fb56e6ba49f0f7b8c012f0a1b6e750ccedbd87f8
Hubert Aschermann <aschehub@fit.cvut.cz>
2022-12-07 14:45:36 +0100

    business: LocationService - findLocation

--- src/main/java/haschman/library_server/business/LocationService.java
+++ src/main/java/haschman/library_server/business/LocationService.java
@@ -5,6 +5,7 @@ import haschman.library_server.domain.Location;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
+import java.util.Optional;
 
 @Component
 public class LocationService extends AbstractCrudService<Location, Long> {
@@ -12,13 +13,19 @@ public class LocationService extends AbstractCrudService<Location, Long> {
         super(repository);
     }
 
+    public Optional<Location> findLocation(int stand, int shelf) {
+        Location intruder = new Location(stand, shelf);
+        Collection<Location> locations = repository.findAll();
+        for (var member : locations)
+            if (intruder.equals(member))
+                return Optional.of(intruder);
+        return Optional.empty();
+    }
+
     @Override
     public Location create(Location location) throws EntityStateException {
-        Collection<Location> locations = repository.findAll();
-        for (var loc : locations) {
-            if (location.equals(loc))
-                throw new EntityStateException(location, " already exists");
-        }
+        if (findLocation(location.getStand(), location.getShelf()).isPresent())
+            throw new EntityStateException(location, " already exists");
         return repository.save(location);
     }
 }
